0ed1c8026fa24ee79fc96574fcfaf11b799728f5
Chris Greening <chris@cmgresearch.com>
2021-11-10 21:23:55 +0000

    Fix running out of memory

--- i2s_sampling/src/main.cpp
+++ i2s_sampling/src/main.cpp
@@ -13,8 +13,8 @@ ADCSampler *adcSampler = NULL;
 I2SSampler *i2sSampler = NULL;
 
 // replace this with your machines IP Address
-#define ADC_SERVER_URL "http://10.0.1.18:5003/adc_samples"
-#define I2S_SERVER_URL "http://10.0.1.18:5003/i2s_samples"
+#define ADC_SERVER_URL "http://192.168.1.72:5003/adc_samples"
+#define I2S_SERVER_URL "http://192.168.1.72:5003/i2s_samples"
 
 // i2s config for using the internal ADC
 i2s_config_t adcI2SConfig = {
@@ -51,6 +51,9 @@ i2s_pin_config_t i2sPins = {
     .data_out_num = I2S_PIN_NO_CHANGE,
     .data_in_num = GPIO_NUM_33};
 
+// how many samples to read at once
+const int SAMPLE_SIZE = 8192;
+
 // send data to a remote address
 void sendData(WiFiClient *wifiClient, HTTPClient *httpClient, const char *url, uint8_t *bytes, size_t count)
 {
@@ -67,10 +70,15 @@ void sendData(WiFiClient *wifiClient, HTTPClient *httpClient, const char *url, u
 void adcWriterTask(void *param)
 {
   I2SSampler *sampler = (I2SSampler *)param;
-  int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * 32768);
+  int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * SAMPLE_SIZE);
+  if (!samples)
+  {
+    Serial.println("Failed to allocate memory for samples");
+    return;
+  }
   while (true)
   {
-    int samples_read = sampler->read(samples, 32768);
+    int samples_read = sampler->read(samples, SAMPLE_SIZE);
     sendData(wifiClientADC, httpClientADC, ADC_SERVER_URL, (uint8_t *)samples, samples_read * sizeof(uint16_t));
   }
 }
@@ -79,10 +87,15 @@ void adcWriterTask(void *param)
 void i2sMemsWriterTask(void *param)
 {
   I2SSampler *sampler = (I2SSampler *)param;
-  int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * 32768);
+  int16_t *samples = (int16_t *)malloc(sizeof(uint16_t) * SAMPLE_SIZE);
+  if (!samples)
+  {
+    Serial.println("Failed to allocate memory for samples");
+    return;
+  }
   while (true)
   {
-    int samples_read = sampler->read(samples, 32768);
+    int samples_read = sampler->read(samples, SAMPLE_SIZE);
     sendData(wifiClientI2S, httpClientI2S, I2S_SERVER_URL, (uint8_t *)samples, samples_read * sizeof(uint16_t));
   }
 }
@@ -91,14 +104,16 @@ void setup()
 {
   Serial.begin(115200);
   // launch WiFi
+  Serial.printf("Connecting to WiFi");
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+    Serial.print(".");
+    delay(1000);
   }
+  Serial.println("");
+  Serial.println("WiFi Connected");
   Serial.println("Started up");
   // indicator LED
   pinMode(2, OUTPUT);
@@ -116,18 +131,17 @@ void setup()
 
   // set up the adc sample writer task
   // TaskHandle_t adcWriterTaskHandle;
-  // xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler, 1, &adcWriterTaskHandle, 1);
   // adcSampler->start();
+  // xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, adcSampler, 1, &adcWriterTaskHandle, 1);
 
   // Direct i2s input from INMP441 or the SPH0645
-  // i2sSampler = new I2SMEMSSampler(I2S_NUM_0, i2sPins, i2sMemsConfigLeftChannel, false);
-
-  // // set up the i2s sample writer task
-  // TaskHandle_t i2sMemsWriterTaskHandle;
-  // xTaskCreatePinnedToCore(i2sMemsWriterTask, "I2S Writer Task", 4096, i2sSampler, 1, &i2sMemsWriterTaskHandle, 1);
+  i2sSampler = new I2SMEMSSampler(I2S_NUM_0, i2sPins, i2sMemsConfigLeftChannel, false);
+  i2sSampler->start();
+  // set up the i2s sample writer task
+  TaskHandle_t i2sMemsWriterTaskHandle;
+  xTaskCreatePinnedToCore(i2sMemsWriterTask, "I2S Writer Task", 4096, i2sSampler, 1, &i2sMemsWriterTaskHandle, 1);
 
   // // start sampling from i2s device
-  // i2sSampler->start();
 }
 
 void loop()
